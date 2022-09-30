72b3fe68e52ea21763f644a55420155f9bc95387
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-09-30 15:20:45 +0300

    init

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from gtts import gTTS
+from playsound import playsound
+from django.shortcuts import render
+
+
+LANGUAGE = 'en'
+
+
+def index(request):
+    if request.method == 'POST':
+        action = request.POST['meaning']
+
+
+        obj = gTTS(text=action, lang=LANGUAGE, slow=False)
+        obj.save("answer.mp3")
+        playsound("answer.mp3")
+        context = {"answer": True}
+        return render(request, 'app/index.html', context=context)
+    else:
+        context = {"answer": ""}
+        return render(request, 'app/index.html', context=context)
